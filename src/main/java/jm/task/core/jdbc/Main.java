b8ab245ae415c2c9eadb041d7ec25d85654fe1ba
yura-pvl@mail.ru <yura-pvl@mail.ru>
2022-10-15 14:26:48 +0600

    Пройденное задание 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,5 +8,15 @@ public class Main {
     private static final UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
         userService.createUsersTable();
+
+        userService.saveUser("Джек", "Воробей", (byte) 31);
+        userService.saveUser("Джо", "Байден", (byte) 54);
+        userService.saveUser("Владимир", "Иванов", (byte) 74);
+        userService.saveUser("Павел", "Снежок Воля", (byte) 38);
+
+        userService.removeUserById(2);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
