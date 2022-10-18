985a4b86628b89efe32802976bf8a91c90a5557a
yura-pvl@mail.ru <yura-pvl@mail.ru>
2022-10-18 07:13:33 +0600

    Настройка базы данных

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,37 +1,63 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static Connection connection = null;
-    private static Util instance = null;
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String HOST = "jdbc:mysql://localhost:3306/java";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "O0v1T_GUE";
+    private static SessionFactory sessionFactory = null;
 
-    public Util() {
+    public static SessionFactory getConnection() {
         try {
-            if (connection == null || connection.isClosed()) {
-                connection = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/java",
-                        "root",
-                        "O0v1T_GUE");
-            }
-        } catch (SQLException e) {
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", DRIVER)
+                    .setProperty("hibernate.connection.url", HOST)
+                    .setProperty("hibernate.connection.username", LOGIN)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .addAnnotatedClass(User.class);
+
+
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static Util getInstance() {
-        if (instance == null) {
-            instance = new Util();
-        }
-        return instance;
-    }
 
 
 
-    public Connection getConnection() {
-        return connection;
-    }
+    //реализация JDBC
+//    private static Connection connection = null;
+//    private static Util instance = null;
+//
+//    public Util() {
+//        try {
+//            if (connection == null || connection.isClosed()) {
+//                connection = DriverManager.getConnection(HOST, LOGIN, PASSWORD);
+//            }
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    public static Util getInstance() {
+//        if (instance == null) {
+//            instance = new Util();
+//        }
+//        return instance;
+//    }
+//
+//
+//
+//    public Connection getConnection() {
+//        return connection;
+//    }
 }
